0b0b05624dce1aa5abe5140d70ab0322d60c6c50
Elloar <elloa.123.t.nunes@gmail.com>
2025-02-17 09:58:09 -0300

    Add files via upload

---
+++ atividade_17-02/script.js
@@ -0,0 +1,10 @@
+document.getElementById("btn-ativar"). 
+addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "ativar");
+})
+document.getElementById("btn-desativar"). 
+addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "desativar");
+})
+
+
